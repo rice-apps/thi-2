cd16deb251efa94b5a8dfc95573237cbc3913f5f
An Bui <atb6@rice.edu>
2024-10-29 21:24:31 -0500

    finalizing for request

--- thi-app/app/(tabs)/index.tsx
+++ thi-app/app/(tabs)/index.tsx
@@ -1,17 +1,9 @@
-import { Button } from 'react-native-paper'
-import { Text, View } from '../../components/Themed'
+import { View, Text } from 'react-native'
 
 export default function LoginPage() {
   return (
     <View className='justify-center'>
       <Text className='p-4 '>Placeholder login page</Text>
-      <Button
-        className='mt-20'
-        mode='contained'
-        // onPress={() => Alert.alert('Paper button is working!')}
-      >
-        Login
-      </Button>
     </View>
   )
 }
