2efe7b3f94c8bb6412c64ca5c1cc336c1f80cbba
Derick Pascual <derickvanpascual@gmail.com>
2024-11-02 14:16:35 -0500

    Some refactoring

--- server/routes/student.router.ts
+++ server/routes/student.router.ts
@@ -4,10 +4,10 @@ const {wrapMiddleware, wrapController} = require("../helper");
 const {studentController} = require("../controllers");
 const studentValidation = require("../validations/student.validation");
 
-router.post("/create", wrapMiddleware(studentValidation.post), wrapController(studentController.create));
-router.put("/:id", wrapMiddleware(studentValidation.put), wrapController(studentController.update));
+router.post("/create", wrapMiddleware(studentValidation.create), wrapController(studentController.create));
+router.put("/:id", wrapMiddleware(studentValidation.update), wrapController(studentController.update));
 router.delete("/:id", wrapMiddleware(studentValidation.delete), wrapController(studentController.delete))
-router.get("/:id", wrapMiddleware(studentValidation.get), wrapController(studentController.findById));
+router.get("/:id", wrapMiddleware(studentValidation.findById), wrapController(studentController.findById));
 
 
 
--- server/validations/student.validation.ts
+++ server/validations/student.validation.ts
@@ -1,21 +1,21 @@
 import Joi from "joi"
 
 module.exports = {
-    post: Joi.object({
+    create: Joi.object({
         first_name: Joi.string().required(),
         last_name: Joi.string().required(),
         tier: Joi.number().required()
     }),
-    put: Joi.object({
+    update: Joi.object({
         first_name: Joi.string().required(),
         last_name: Joi.string().required(),
         tier: Joi.number().required(),
         id: Joi.string().required(),
     }),
-    get: Joi.object({
+    delete: Joi.object({
         id: Joi.string().required()
     }).unknown(true),
-    delete: Joi.object({
+    findById: Joi.object({
         id: Joi.string().required()
     }).unknown(true),
 };
