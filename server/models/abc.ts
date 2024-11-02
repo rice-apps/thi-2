a839436ee78bf3669a3b9b1715cc0e24baf4f70b
sarahmotteler <sarahmotteler2021@gmail.com>
2024-11-02 15:40:50 -0500

    fix abc schema

--- server/models/abc.ts
+++ server/models/abc.ts
@@ -3,15 +3,15 @@ const { Schema } = mongoose;
 
 // Schema
 const abcSchema = new Schema({
-        datetime: {type: Date, required: true},
-        staff_name: { type: String, required: true },
-        setting: { type: String, required: true },
-        antecedent: { type: String, required: true },
-        behavior: { type: String, required: true },
-        consequence: { type: String, required: true },
-        notes: { type: String },
-        images: { type: Image }
-      });
+        staff: {type: Object, required: true}, // will be replaced by account schema when the branches are merged
+        student_id: {type: Object, required: true}, // will be replaced by student schema when the branches are merged
+        date: {type: Date, required: true}, // note: splitting into date and time will have to be done later since date contains both the date and time
+        settings: {type: String, required: true}, 
+        antecedent: {type: String, required: true}, 
+        behavior: {type: String, required: true}, 
+        consequence: {type: String, required: true}, 
+        comments: {type: String, required: true}, 
+});
       
       // `UserModel` will have `name: string`, etc.
       module.exports = mongoose.model('abc', abcSchema);
