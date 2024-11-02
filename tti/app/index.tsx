f23e6c6990314730926d1f0f81c7ab36121f1497
neyidamichel <nm68@rice.edu>
2024-11-02 15:02:09 -0500

    Login using Tailwind

--- tti/app/index.tsx
+++ tti/app/index.tsx
@@ -12,6 +12,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+
       <Login />
     </View>
   );
--- tti/app/login-componets/login.tsx
+++ tti/app/login-componets/login.tsx
@@ -1,64 +1,5 @@
-import {View, StyleSheet, Text, Image, TextInput, TouchableOpacity  } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        width: '100%',
-    
-    },
-    title: {
-        fontSize: 20,
-        textAlign: 'center',
-        marginBottom: 20,
-        fontFamily: 'sans-serif',
-        color: 'black',
-        fontWeight: 'bold',
-    },
-    subTitle: {
-        fontSize: 16,
-        fontFamily: 'sans-serif',
-        textAlign: 'center',
-        marginBottom: 20,
-        color: 'black',
-        lineHeight: 24,
-        
-    },
-    inputBox: {
-        marginBottom: 15,
-        width: 300,
-        alignSelf: 'center',
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        paddingHorizontal: 10,
-        borderRadius: 5,
-    },
-    button: {
-        alignSelf: 'center',
-        backgroundColor: '#17468F',
-        borderRadius: 5,
-        marginTop: 10,
-        width: 300,
-    },
-    buttonText: {
-        color: 'white',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        fontSize: 16,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    image: {
-        width: 270,
-        height: 121,
-        alignSelf: 'center',
-        margin: 50,
-    }
-});
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -66,64 +7,63 @@ const Login = () => {
 
     const handleLogin = () => {
         console.log('Email:', username);
-      };
+    };
 
     const handleGoogleLogin = () => {
         console.log('Google Login');
-
-    }
+    };
 
     const handleAppleLogin = () => {
         console.log('Apple Login');
-    }
+    };
 
     const handleFacebookLogin = () => {
         console.log('Facebook Login');
-    }
+    };
 
     return (
-        <View style={styles.container}>
+        <View className="flex-1 justify-center bg-white w-full">
             <Image 
-            style ={styles.image} 
-            source={require('../../assets/images/logo.png.png')}
-
+                className="w-68 h-32 self-center my-12"
+                source={require('../../assets/images/logo.png')}
             />
-            <Text style ={styles.title}>Welcome Back!</Text>
-            <TextInput style={styles.subTitle} editable={false} value="Please login to your account to continue."/>
+            <Text className="text-2xl font-bold text-center text-black mb-5">Welcome Back!</Text>
+            <TextInput 
+                className="text-lg text-center mb-5 text-black leading-6" 
+                editable={false} 
+                value="Please login to your account to continue."
+            />
             <TextInput
-                style={styles.inputBox}
+                className="mb-4 w-72 self-center h-10 border border-gray-400 px-3 rounded"
                 placeholder="Enter Your Email Address"
                 onChangeText={setUsername}
                 value={username}
             />
             <TextInput
-                style={styles.inputBox}
+                className="mb-4 w-72 self-center h-10 border border-gray-400 px-3 rounded"
                 placeholder="Enter Your Password"
                 onChangeText={setPassword}
                 value={password}
                 secureTextEntry={true}
             />
 
-            <TouchableOpacity onPress={handleLogin} style={styles.button}>
-                <Text style={styles.buttonText}>Login</Text>
+            <TouchableOpacity onPress={handleLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
+                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={handleGoogleLogin} style={styles.button}>    
-                <Text style={styles.buttonText}>Login with Google</Text>
+            <TouchableOpacity onPress={handleGoogleLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
+                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login with Google</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={handleAppleLogin} style={styles.button}>
-                <Text style={styles.buttonText}>Login with Apple</Text>
+            <TouchableOpacity onPress={handleAppleLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
+                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login with Apple</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={handleFacebookLogin} style={styles.button}>
-                <Text style={styles.buttonText}>Login with Facebook</Text>
+            <TouchableOpacity onPress={handleFacebookLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
+                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login with Facebook</Text>
             </TouchableOpacity>
-        
         </View>
-        
-);
+    );
 };
-
 
 export default Login;
