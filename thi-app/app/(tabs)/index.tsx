621b6dd2b325f49846a5e13444fce53cd6ebfa13
neyidamichel <nm68@rice.edu>
2024-11-02 15:31:03 -0500

    Confgig. and deleting unusable parts

--- thi-app/app/(tabs)/index.tsx
+++ thi-app/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'
+import Login from '../../components/login'
 
 export default function LoginPage() {
   return (
     <View className='justify-center'>
-      <Text className='p-4 '>Placeholder login page</Text>
+      <Login />
+
     </View>
   )
 }
