387a9a1319cbc3a757409e16d1fdcc2e9039eaf9
trietmchau <trietmchau@gmail.com>
2024-11-03 03:25:22 -0600

    Add jwtAuthen

--- server/routes/auth.router.ts
+++ server/routes/auth.router.ts
@@ -3,7 +3,9 @@ const router = express.Router();
 const {wrapMiddleware, wrapController} = require("../helper");
 const {authController} = require("../controllers");
 const authValidation = require("../validations/auth.validation");
-const jwtAuthen = require("../validations/jwtAuthentication");
+const {jwtAuthen} = require("../validations")
+
+
 // Template controller
 router.post('/sign-up', wrapMiddleware(authValidation.signUp), wrapController(authController.signUp));
 
