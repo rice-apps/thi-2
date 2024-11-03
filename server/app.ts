c3aed62852be34e5f3664407f6520dbf89b23486
trietmchau <trietmchau@gmail.com>
2024-11-03 03:22:53 -0600

    Add ErrorHandler endpoint

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+require("dotenv").config();
 const cors = require("cors");
 const morgan = require("morgan");
-const helmet = require("helmet");
-const rateLimit = require('express-rate-limit');
-const port = 3000;
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT;
+
 const { default: mongoose } = require("mongoose");
 
 const uri = 
@@ -15,32 +13,33 @@ const uri =
   process.env.MONGO_ADMIN_PASSWORD +
   "@thi-cluster.nkv5u.mongodb.net/thi-behavior?retryWrites=true&w=majority&appName=thi-cluster";
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.',
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+
 async function run() {
     try {
-      await mongoose.connect(uri, {serverSelectionTimeoutMS: 5000});
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      const app = express();
-      
-      app.use(express.json());
-      app.use(cors());
-      app.use(helmet());
-      app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms'));
-      // app.use(limiter);
-      app.use('/api', require("./routes"));
-  
-      app.listen(port, () => {
-        console.log(`App listening at http://localhost:${port}`);
-      });
+        await mongoose.connect(uri, {serverSelectionTimeoutMS: 5000});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const app = express();
+        
+        app.use(cors());
+        app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms'));
+        app.use(express.json());
+        app.use('/api', require("./routes"));
+        
+        app.use(errorHandle);
+
+        app.listen(port, () => {
+            console.log(`App listening at http://localhost:${port}`);
+        });
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  
-  run().catch(console.dir);
+}
+
+const errorHandle = (error: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.statusCode || 401).json({
+        success: false,
+        statusCode: error.statusCode || 401,
+        message: error.message,
+    })
+}
+run().catch(console.dir);
