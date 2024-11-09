13c8a3be99fa0625723410cf3b0034582ee83747
Derick Pascual <derickvanpascual@gmail.com>
2024-11-09 15:17:34 -0600

    Some duration changes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,6 @@
 const authRouter = require("./auth.router");
-const studentRouter = require("./student.router");
 const durationRouter = require("./duration.router");
+const studentRouter = require("./student.router");
 const abcRouter = require("./abc.router");
 import express from "express";
 const router = express.Router();
