e0a066130b7d61992f98e35c5c2fde1cb6075a4b
Derick Pascual <derickvanpascual@gmail.com>
2024-11-09 15:17:34 -0600

    Some duration changes

--- server/controllers/duration.controller.ts
+++ server/controllers/duration.controller.ts
@@ -17,13 +17,52 @@ class DurationController {
     }
 
     async update(req: Request, res: Response, next: NextFunction) {
-        const id = req.params.student_id;
+        try {
+            const studentId = req.params.student_id;
+            const newData = req.body;
+            const updatedDuration = await Duration.findOneAndUpdate(
+                { student_id: studentId },
+                newData
+            );
 
-        const newData = req.body;
+            if (!updatedDuration) {
+                throw new ErrorResponse({
+                    statusCode: HttpStatus.StatusCodes.NOT_FOUND,
+                    message: `Student with id ${studentId} not found.`
+                })
+            }
+
+            return updatedDuration;
+        } catch (err: any) {
+            throw err;
+        }
+
     }
 
     async delete(req: Request, res: Response, next: NextFunction) {
+        try {
+            const studentId = req.params.student_id;
+            
+            if (!mongoose.Types.ObjectId.isValid(studentId)) {
+                throw new ErrorResponse({
+                    statusCode: HttpStatus.StatusCodes.BAD_REQUEST,
+                    message: `Invalid studentId format: ${studentId}.`
+                });
+            }
 
+            const deletedDuration = await Duration.findOneAndDelete({ student_id: studentId });
+
+            if (!deletedDuration) {
+                throw new ErrorResponse({
+                    statusCode: HttpStatus.StatusCodes.NOT_FOUND,
+                    message: `Student with id ${studentId} not found.`,
+                });
+            }
+
+            return deletedDuration;
+        } catch (err: any) {
+
+        }
     }
 
     async findAll(req: Request, res: Response, next: NextFunction) {
--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,9 +1,7 @@
 const authController = require("./auth.controller");
-const studentController = require("./student.controller")
 const durationController = require("./duration.controller")
 
 module.exports = {
     authController,
-    studentController,
     durationController
 }
