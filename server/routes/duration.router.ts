3bf86635c11eb797d5aa35f1a969c446a74ab0fe
Derick Pascual <derickvanpascual@gmail.com>
2024-11-09 14:23:23 -0600

    changes

--- server/routes/duration.router.ts
+++ server/routes/duration.router.ts
@@ -1,18 +1,38 @@
 import express from "express";
 const router = express.Router();
-const {wrapMiddleware, wrapController} = require("../helper");
-const {durationController} = require("../controllers");
+const { wrapMiddleware, wrapController } = require("../helper");
+const { durationController } = require("../controllers");
 const durationValidation = require("../validations/duration.validation");
 
-router.post("/create", wrapMiddleware(durationValidation.create), wrapController(durationController.create));
-router.put("/:student_id", wrapMiddleware(durationValidation.update), wrapController(durationController.update));
-router.delete("/:student_id", wrapMiddleware(durationValidation.delete), wrapController(durationController.delete));
-router.get("/all", wrapMiddleware(durationValidation.findAll), wrapController(durationController.findAll));
-<<<<<<< HEAD
-router.get("/:student_id", wrapMiddleware(durationValidation.findByStudentId), wrapController(durationController.findByStudentId));
-router.get("/staff/:staff_id", wrapMiddleware(durationValidation.findByStaffId), wrapController(durationController.findByStaffId));
-=======
-router.get("/:student_id", wrapMiddleware(durationValidation.findById, wrapController(durationValidation.findById)));
->>>>>>> 2f3c096 (Added routers, controllers, validation structure for duration)
+router.post(
+    "/create",
+    wrapMiddleware(durationValidation.create),
+    wrapController(durationController.create)
+);
+router.put(
+    "/:student_id",
+    wrapMiddleware(durationValidation.update),
+    wrapController(durationController.update)
+);
+router.delete(
+    "/:student_id",
+    wrapMiddleware(durationValidation.delete),
+    wrapController(durationController.delete)
+);
+router.get(
+    "/all",
+    wrapMiddleware(durationValidation.findAll),
+    wrapController(durationController.findAll)
+);
+router.get(
+    "/staff/:staff_id",
+    wrapMiddleware(durationValidation.findByStaffId),
+    wrapController(durationController.findByStaffId)
+);
+router.get(
+    "/:student_id",
+    wrapMiddleware(durationValidation.findById),
+    wrapController(durationController.findByStudentId)
+);
 
 module.exports = router;
