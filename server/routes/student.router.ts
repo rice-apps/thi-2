ce7ea7f03a746ed0682cebeb8f1d96fe7caeb81f
Derick Pascual <derickvanpascual@gmail.com>
2024-11-09 14:37:36 -0600

    added auth

--- server/routes/student.router.ts
+++ server/routes/student.router.ts
@@ -3,11 +3,12 @@ const router = express.Router();
 const {wrapMiddleware, wrapController} = require("../helper");
 const {studentController} = require("../controllers");
 const studentValidation = require("../validations/student.validation");
+const jwtAuthen = require("../validations/jwtAuthen");
 
-router.post("/create", wrapMiddleware(studentValidation.create), wrapController(studentController.create));
-router.put("/:id", wrapMiddleware(studentValidation.update), wrapController(studentController.update));
-router.delete("/:id", wrapMiddleware(studentValidation.delete), wrapController(studentController.delete))
-router.get("/:id", wrapMiddleware(studentValidation.findById), wrapController(studentController.findById));
+router.post("/create", jwtAuthen, wrapMiddleware(studentValidation.create), wrapController(studentController.create));
+router.put("/:id", jwtAuthen, wrapMiddleware(studentValidation.update), wrapController(studentController.update));
+router.delete("/:id", jwtAuthen, wrapMiddleware(studentValidation.delete), wrapController(studentController.delete))
+router.get("/:id", jwtAuthen, wrapMiddleware(studentValidation.findById), wrapController(studentController.findById));
 
 
 
