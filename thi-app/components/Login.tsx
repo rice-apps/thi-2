22d394bdd31e639d9020a31ca239ef6da9f28377
neyidamichel <nm68@rice.edu>
2024-11-16 15:28:02 -0600

    Done with formattinglogin- Needs Auth and Paths

--- thi-app/components/Login.tsx
+++ thi-app/components/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 import { useRouter } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons'; 
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -13,65 +14,77 @@ const Login = () => {
         router.replace('/(drawer)/home'); // Temporary homepage redirect
     };
 
-    const handleGoogleLogin = () => {
-        console.log('Google Login');
-        router.replace('/(drawer)/home'); // Temporary homepage redirect
-    };
-
-    const handleAppleLogin = () => {
-        console.log('Apple Login');
-        router.replace('/(drawer)/home'); // Temporary homepage redirect
-    };
-
-    const handleFacebookLogin = () => {
-        console.log('Facebook Login');
+    
+    const handleSocialLogin = () => {
+        console.log(`Login`);
         router.replace('/(drawer)/home'); // Temporary homepage redirect
     };
 
     return (
-        <View className="flex-1 justify-center bg-white w-full">
+        <View className="flex-1 justify-center bg-white px-4 items-center">
+             {/* Logo */}
             <Image 
-                className="w-68 h-32 self-center my-12"
-                source={require('../assets/images/logo.png')}
+                className="self-center my-10"
+                source={require('../assets/images/bb_icon.png')}
                 style={{
-                    height: Dimensions.get('window').height * 0.3,
-                    width: Dimensions.get('window').width * 0.4,
+                    height: Dimensions.get('window').height * 0.2,
+                    width: Dimensions.get('window').width * 0.18,
                 }}
             />
-            <Text className="text-2xl font-bold text-center text-black mb-5">Welcome Back!</Text>
-            <TextInput 
-                className="text-lg text-center mb-5 text-black leading-6" 
-                editable={false} 
-                value="Please login to your account to continue."
-            />
+             {/* Welcome */}
+            <Text className="text-2xl font-bold text-center text-black mb-2">Welcome Back!</Text>
+            
+            <Text className="text-base text-center text-black-600 mb-6">
+                Please login to your account to continue.
+            </Text>
+
             <TextInput
-                className="mb-4 w-72 self-center h-10 border border-gray-400 px-3 rounded"
+        
+                className="mb-4 h-12 border border-gray-300 px-3 rounded-md text-black w-4/5 max-w-md"
                 placeholder="Enter Your Email Address"
                 onChangeText={setUsername}
                 value={username}
             />
             <TextInput
-                className="mb-4 w-72 self-center h-10 border border-gray-400 px-3 rounded"
+                className="mb-4 h-12 border border-gray-300 px-3 rounded-md text-black w-4/5 max-w-md"
                 placeholder="Enter Your Password"
                 onChangeText={setPassword}
                 value={password}
                 secureTextEntry={true}
             />
-
-            <TouchableOpacity onPress={handleLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
-                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login</Text>
+            {/* Login Button */}
+            <TouchableOpacity onPress={handleLogin} className="bg-[#105366] h-10 rounded-md py-2 w-4/5 max-w-md mb-4 justify-center items-center">
+                <Text className="text-white text-md font-bold text-center">Login</Text>
             </TouchableOpacity>
+           
+            {/* Divider */}
+            <View className="flex-row items-center mb-4 w-4/5 max-w-md">
+                <View className="flex-1 h-px bg-gray-300"></View>
+                <Text className="px-4 text-lg text-black-500">Or</Text>
+                <View className="flex-1 h-px bg-gray-300"></View>
+            </View>
 
-            <TouchableOpacity onPress={handleGoogleLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
-                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login with Google</Text>
+            {/* Social Media Logins */}
+            <TouchableOpacity
+                onPress={() => handleSocialLogin()}
+                className="flex-row bg-white border border-gray-300 rounded-md py-3 w-11/12 max-w-md mb-4 items-center justify-center"
+            >
+                <FontAwesome name="google" size={20} color="#DB4437" className="mr-2" />
+                <Text className="text-black text-base font-medium text-center">Sign in with Google</Text>
             </TouchableOpacity>
-
-            <TouchableOpacity onPress={handleAppleLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
-                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login with Apple</Text>
+            <TouchableOpacity
+                onPress={() => handleSocialLogin()}
+                className="flex-row bg-white border border-gray-300 rounded-md py-3 w-11/12 max-w-md mb-4 items-center justify-center"
+            >
+                <FontAwesome name="apple" size={20} color="#000" className="mr-2" />
+                <Text className="text-black text-base font-medium text-center">Sign in with Apple</Text>
             </TouchableOpacity>
-
-            <TouchableOpacity onPress={handleFacebookLogin} className="bg-blue-800 rounded mt-3 w-72 self-center">
-                <Text className="text-white py-2 px-5 text-lg font-bold text-center">Login with Facebook</Text>
+            <TouchableOpacity
+                onPress={() => handleSocialLogin()}
+                className="flex-row bg-white border border-gray-300 rounded-md py-3 w-11/12 max-w-md items-center justify-center"
+            >
+                <FontAwesome name="facebook" size={20} color="#4267B2" className="mr-2" />
+                <Text className="text-black text-base font-medium text-center">Sign in with Facebook</Text>
             </TouchableOpacity>
         </View>
     );
