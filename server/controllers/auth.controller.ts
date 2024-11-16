6f09eaf60854703277620532ad14d89a954c05f0
Michael Yu <13585954208@189.cn>
2024-11-16 14:02:49 -0600

    update

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -4,7 +4,29 @@ const HttpStatus = require("http-status-codes");
 const { ErrorResponse } = require("../helper");
 
 class AuthController {
-  // For reference purposes only, NOT actual implementation
+  async createUser(req: Request, res: Response, next: NextFunction) {
+    const { email, password, first_name, last_name } = req.body;
+    const account = new Account({
+      email: email,
+      password: password,
+      first_name: first_name,
+      last_name: last_name,
+      is_admin: true,
+      is_active: true,
+      is_deleted: true,
+      authorization_token: "test",
+    });
+    try {
+      await account.save();
+      return { message: "Request processed!", id: account._id };
+    } catch (error) {
+      throw new ErrorResponse({
+        statusCode: HttpStatus.StatusCodes.BAD_REQUEST,
+        message: error,
+      });
+    }
+  }
+  //controller for signing up a user
   async signUp(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
     if (email.length > 20) {
@@ -14,16 +36,6 @@ class AuthController {
       });
     }
 
-    const account = new Account({
-      email: email,
-      password: password,
-      first_name: "test",
-      last_name: "test",
-      is_admin: true,
-      is_active: true,
-      is_deleted: true,
-      authorization_token: "test",
-    });
     try {
       await account.save();
       return { message: "Request processed!", id: account._id };
--- server/routes/auth.router.ts
+++ server/routes/auth.router.ts
@@ -3,13 +3,61 @@ const router = express.Router();
 const { wrapMiddleware, wrapController } = require("../helper");
 const { authController } = require("../controllers");
 const authValidation = require("../validations/auth.validation");
-// Template controller
+// Sign Up Router
 router.post(
   "/sign-up",
   wrapMiddleware(authValidation.signUp),
   wrapController(authController.signUp)
 );
-//router.post("/set-password", wrapMiddleware(authValida));
+// Router for setting password
+router.post(
+  "/set-password",
+  wrapMiddleware(authValidation.setPassword),
+  wrapController(authController.setPassword)
+);
+//Router for signing in
+router.post(
+  "/sign-in",
+  wrapMiddleware(authValidation.signIn),
+  wrapController(authController.signIn)
+);
+//Router for deleting account
+router.delete(
+  "/drop-account",
+  wrapMiddleware(authValidation.dropAccount),
+  wrapController(authController.dropAccount)
+);
+//Router for changing password (change for a new password)
+router.put(
+  "/change-password",
+  wrapMiddleware(authValidation.changePassword),
+  wrapController(authController.changePassword)
+);
+//Router for forgetting password (setting a new password)
+router.post(
+  "/set-password",
+  wrapMiddleware(authValidation.forgetPassword),
+  wrapController(authController.forgetPassword)
+);
+//Router for updating info
+router.post(
+  "/update-info",
+  wrapMiddleware(authValidation.updateInfo),
+  wrapController(authController.updateInfo)
+);
+//Router for getting user's info
+router.post(
+  "/info",
+  wrapMiddleware(authValidation.info),
+  wrapController(authController.info)
+);
+//Router for logging a user out
+router.post(
+  "/log-out",
+  wrapMiddleware(authValidation.logOut),
+  wrapController(authController.logOut)
+);
+
 /**
  * Set password (POST)
 Sign in (POST)
@@ -21,4 +69,5 @@ Info (POST)
 Log out (POST)
 Remove account (DELETE)
  */
+
 module.exports = router;
