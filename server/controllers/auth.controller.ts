bb30993d5a0df5e087b0eecd4cd5e0d0d70c10d3
trietmchau <trietmchau@gmail.com>
2024-11-16 14:10:37 -0600

    Add

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -1,8 +1,7 @@
-
 import { NextFunction, Request, Response } from "express";
-const {Account} = require("../models");
+const { Account } = require("../models");
 const HttpStatus = require("http-status-codes");
-const {ErrorResponse, resendService, emailFormat} = require("../helper");
+const { ErrorResponse, resendService, emailFormat } = require("../helper");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
@@ -14,7 +13,7 @@ class AuthController {
             throw new ErrorResponse({
                 statusCode: HttpStatus.StatusCodes.BAD_REQUEST,
                 message: "Email too long (20 char limit)",
-            })
+            });
         }
 
         const account = new Account({
@@ -22,43 +21,53 @@ class AuthController {
             password: password,
             first_name: "John",
             last_name: "Doe",
-            is_deleted: true
+            is_deleted: true,
         });
         try {
             await account.save();
             resendService.emails.send(emailFormat("tmc15@rice.edu"));
-            return {message: "Request processed!", id: account._id};
+            return { message: "Request processed!", id: account._id };
         } catch (error) {
             throw new ErrorResponse({
                 statusCode: HttpStatus.StatusCodes.BAD_REQUEST,
                 message: error,
-            })
+            });
         }
     }
 
     async signIn(req: Request, res: Response, next: NextFunction) {
         const { email, password } = req.body;
-        const account = await Account.findOne({email: email}, "_id password is_admin is_deleted").exec();
-        console.log(account.is_deleted)
+        const account = await Account.findOne(
+            { email: email },
+            "_id password is_admin is_deleted"
+        ).exec();
+        console.log(account.is_deleted);
         if (!account || account.is_deleted) {
             throw new ErrorResponse({
                 statusCode: HttpStatus.StatusCodes.BAD_REQUEST,
                 message: "ACCOUNT NOT FOUND",
-            })
+            });
         }
         if (account.password == password) {
-            return {message: "Signed in!", token: jwt.sign({id: account._id}, 
-                process.env.JWT_SECRET_KEY, {expiresIn: 86400})}
+            return {
+                message: "Signed in!",
+                token: jwt.sign(
+                    { id: account._id },
+                    process.env.JWT_SECRET_KEY,
+                    { expiresIn: 86400 }
+                ),
+            };
         } else {
             throw new ErrorResponse({
                 statusCode: HttpStatus.StatusCodes.BAD_REQUEST,
                 message: "WRONG PASSWORD",
-            })
+            });
         }
     }
 
     async changePassword(req: any, res: Response, next: NextFunction) {
-        return {message: "Success", ...req.user};
+        console.log(req.user);
+        return { message: "Success", ...req.user };
     }
 }
 
