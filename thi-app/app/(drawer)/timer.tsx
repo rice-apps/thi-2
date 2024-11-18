2a2bd374d78f3f9025bea4e71ad96734ca4d7411
AB-102 <61863411+AB-102@users.noreply.github.com>
2024-11-18 02:24:53 -0600

    zzz

--- thi-app/app/(drawer)/timer.tsx
+++ thi-app/app/(drawer)/timer.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import ImageTimer from '@/components/AnimatedImageTimer';
+import { useSharedValue } from 'react-native-reanimated';
 
 export default function TimerPage() {
+  // Tie animations to initial sidebar state (currently set to open)
+  const timerAnimatedValue = useSharedValue(1);
+  
   return (
     <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text>Placeholder timer page</Text>
+      <ImageTimer imageName={"tiger"}, animatedValue={timerAnimatedValue} />
+      {/* <Text>Placeholder timer page</Text> */}
     </View>
   );
 }
