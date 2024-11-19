9874d31e0779621c1b73e5efbd43698f444ee979
AB-102 <61863411+AB-102@users.noreply.github.com>
2024-11-19 11:32:01 -0600

    style formatting

--- thi-app/app/(drawer)/_layout.tsx
+++ thi-app/app/(drawer)/_layout.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import { View, Dimensions, SafeAreaView } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
-import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-import { Slot } from 'expo-router';
-import Sidebar, { SidebarContext, useSidebarContext, useTransitionCustomization } from '@/components/Sidebar';
+import React, { useState } from 'react'
+import { View, Dimensions, SafeAreaView } from 'react-native'
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated'
+import { GestureDetector, Gesture } from 'react-native-gesture-handler'
+import { Slot } from 'expo-router'
+import Sidebar, {
+  SidebarContext,
+  useSidebarContext,
+  useTransitionCustomization,
+} from '@/components/Sidebar'
 
 const DrawerLayout = () => {
   // Sidebar state, dimensions, and transition settings
-  const { transitionEasing, transitionDuration } = useTransitionCustomization();
-  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isTransitioning, setIsTransitioning] = useState(false);
+  const { transitionEasing, transitionDuration } = useTransitionCustomization()
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isTransitioning, setIsTransitioning] = useState(false)
   const {
     openSidebarWidth,
     closedSidebarWidth,
@@ -20,99 +24,106 @@ const DrawerLayout = () => {
     defaultTabColor,
     buttonColor,
     buttonSize,
-  } = useSidebarContext();
+  } = useSidebarContext()
   // Tie animations to initial sidebar state (currently set to open)
-  const sidebarAnimatedValue = useSharedValue(isSidebarOpen ? 1 : 0);
+  const sidebarAnimatedValue = useSharedValue(isSidebarOpen ? 1 : 0)
   const mainScreenWidth = useSharedValue(
-    Dimensions.get('window').width - (openSidebarWidth + closedSidebarWidth));
+    Dimensions.get('window').width - (openSidebarWidth + closedSidebarWidth)
+  )
 
   const toggleSidebar = async () => {
-    if ((sidebarAnimatedValue.value === 1 && isSidebarOpen) ||
-    (sidebarAnimatedValue.value === 0 && !isSidebarOpen)) {
-      setIsTransitioning(true);
-      setIsSidebarOpen(!isSidebarOpen);
-      transitionSidebar();
-      transitionMainScreen();
+    if (
+      (sidebarAnimatedValue.value === 1 && isSidebarOpen) ||
+      (sidebarAnimatedValue.value === 0 && !isSidebarOpen)
+    ) {
+      setIsTransitioning(true)
+      setIsSidebarOpen(!isSidebarOpen)
+      transitionSidebar()
+      transitionMainScreen()
       // Allow swipes after complete transition
-      await delay(transitionDuration);
-      setIsTransitioning(false);
+      await delay(transitionDuration)
+      setIsTransitioning(false)
     }
-  };
+  }
 
   // Set main screen dynamic width
   const mainScreenAnimatedStyle = useAnimatedStyle(() => {
-    return { width: mainScreenWidth.value };
-  });
+    return { width: mainScreenWidth.value }
+  })
 
   // Transitions sidebar for 400ms duration
   const transitionSidebar = () => {
     sidebarAnimatedValue.value = withTiming(isSidebarOpen ? 0 : 1, {
-        duration: transitionDuration,
-        easing: transitionEasing,
-      }
-    );
-  };
+      duration: transitionDuration,
+      easing: transitionEasing,
+    })
+  }
 
   // Transitions main screen for 400ms duration
   const transitionMainScreen = () => {
-    mainScreenWidth.value = withTiming(isSidebarOpen ?
-      Dimensions.get('window').width - closedSidebarWidth : 
-      Dimensions.get('window').width - (openSidebarWidth + closedSidebarWidth),
-        {
-          duration: transitionDuration,
-          easing: transitionEasing,
-        }
-    );
-  };
+    mainScreenWidth.value = withTiming(
+      isSidebarOpen
+        ? Dimensions.get('window').width - closedSidebarWidth
+        : Dimensions.get('window').width - (openSidebarWidth + closedSidebarWidth),
+      {
+        duration: transitionDuration,
+        easing: transitionEasing,
+      }
+    )
+  }
 
   // Horizontal swipe triggers sidebar toggle
-  const swipeGesture = Gesture.Pan()
-  .onUpdate((event) => {
+  const swipeGesture = Gesture.Pan().onUpdate((event) => {
     // Ignore swipes mid-transition
-    if (isTransitioning) return;
+    if (isTransitioning) return
     // Threshold horizontal distance is 50 px to trigger
-    if ((event.translationX > 50 && !isSidebarOpen)||(event.translationX < -50 && isSidebarOpen)) {
-      toggleSidebar();
+    if (
+      (event.translationX > 50 && !isSidebarOpen) ||
+      (event.translationX < -50 && isSidebarOpen)
+    ) {
+      toggleSidebar()
     }
-  });
+  })
 
   return (
     <SafeAreaView>
-      <SidebarContext.Provider value={{
-        isSidebarOpen,
-        toggleSidebar,
-        openSidebarWidth,
-        closedSidebarWidth,
-        activeIconTextColor,
-        defaultIconTextColor,
-        activeTabColor,
-        defaultTabColor,
-        buttonColor,
-        buttonSize,
-        }}>
+      <SidebarContext.Provider
+        value={{
+          isSidebarOpen,
+          toggleSidebar,
+          openSidebarWidth,
+          closedSidebarWidth,
+          activeIconTextColor,
+          defaultIconTextColor,
+          activeTabColor,
+          defaultTabColor,
+          buttonColor,
+          buttonSize,
+        }}
+      >
         <GestureDetector gesture={swipeGesture}>
-          <View className="flex-1 flex-row" collapsable={false}>
-            
+          <View className='flex-1 flex-row' collapsable={false}>
             {/* Sidebar */}
-            <Sidebar animatedValue={sidebarAnimatedValue}/>
-            
-            {/* Main screen in ./(drawer)/ */}
-            <Animated.View style={[
-              mainScreenAnimatedStyle,
-              {
-                position: 'absolute',
-                right: 0,
-                height: Dimensions.get('window').height,
-                zIndex: 0, // Covered by sidebar layer
-              },
-            ]}>
-                <Slot />
-            </Animated.View>
+            <Sidebar animatedValue={sidebarAnimatedValue} />
 
+            {/* Main screen in ./(drawer)/ */}
+            <Animated.View
+              style={[
+                mainScreenAnimatedStyle,
+                {
+                  position: 'absolute',
+                  right: 0,
+                  height: Dimensions.get('window').height,
+                  zIndex: 0, // Covered by sidebar layer
+                },
+              ]}
+            >
+              <Slot />
+            </Animated.View>
           </View>
         </GestureDetector>
       </SidebarContext.Provider>
     </SafeAreaView>
-  );
-};
-export default DrawerLayout;
+  )
+}
+export default DrawerLayout
--- thi-app/components/Sidebar.tsx
+++ thi-app/components/Sidebar.tsx
@@ -1,150 +1,195 @@
-import React, { createContext, useContext } from 'react';
-import { View, Image, Dimensions, Pressable, ViewStyle } from 'react-native';
-import Animated, { SharedValue, useAnimatedStyle, interpolate, FadeInLeft, FadeOutLeft, Easing, AnimatedStyle } from 'react-native-reanimated';
-import { Entypo, FontAwesome, FontAwesome6, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import SidebarTab, { dynamicIconSize } from '@/components/SidebarTab';
+import React, { createContext, useContext } from 'react'
+import { View, Image, Dimensions, Pressable, ViewStyle } from 'react-native'
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  interpolate,
+  FadeInLeft,
+  FadeOutLeft,
+  Easing,
+  AnimatedStyle,
+} from 'react-native-reanimated'
+import {
+  Entypo,
+  FontAwesome,
+  FontAwesome6,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons'
+import SidebarTab, { dynamicIconSize } from '@/components/SidebarTab'
 
 // Sidebar state, dimensions, and customization
 export const SidebarContext = createContext({
-    isSidebarOpen: true,
-    toggleSidebar: () => {},
-    openSidebarWidth: Dimensions.get('window').width * 0.18,
-    closedSidebarWidth: Dimensions.get('window').width * 0.02,
-    activeIconTextColor: 'white',
-    defaultIconTextColor: 'black',
-    activeTabColor: '#10536699',
-    defaultTabColor: 'transparent',
-    buttonColor: '#105366',
-    buttonSize: 2.3, // in rem (1 rem = 16px)
-});
+  isSidebarOpen: true,
+  toggleSidebar: () => {},
+  openSidebarWidth: Dimensions.get('window').width * 0.18,
+  closedSidebarWidth: Dimensions.get('window').width * 0.02,
+  activeIconTextColor: 'white',
+  defaultIconTextColor: 'black',
+  activeTabColor: '#10536699',
+  defaultTabColor: 'transparent',
+  buttonColor: '#105366',
+  buttonSize: 2.3, // in rem (1 rem = 16px)
+})
 
 // Customize transition settings
 const TransitionCustomization = createContext({
-    transitionEasing: Easing.out(Easing.cubic),
-    transitionDuration: 400, // in ms
+  transitionEasing: Easing.out(Easing.cubic),
+  transitionDuration: 400, // in ms
 })
 
-export const useSidebarContext = () => useContext(SidebarContext);
-export const useTransitionCustomization = () => useContext(TransitionCustomization);
+export const useSidebarContext = () => useContext(SidebarContext)
+export const useTransitionCustomization = () => useContext(TransitionCustomization)
 
 // Open/close sidebar button
 const SidebarButton = ({ animatedStyle }: { animatedStyle: AnimatedStyle<ViewStyle> }) => {
-    const {
-        toggleSidebar,
-        openSidebarWidth,
-        closedSidebarWidth,
-        buttonColor,
-        buttonSize,
-    } = useSidebarContext();
+  const { toggleSidebar, openSidebarWidth, closedSidebarWidth, buttonColor, buttonSize } =
+    useSidebarContext()
 
-    return (
-        <Pressable className="absolute top-[8%] items-center justify-center aspect-square rounded-full shadow-lg" style={{
-            width: buttonSize * 16,
-            backgroundColor: buttonColor,
-            left: openSidebarWidth + closedSidebarWidth - (buttonSize * 16 / 2)
-        }} onPress={toggleSidebar}>
-            <Animated.View style={animatedStyle}>
-                <MaterialIcons name="keyboard-arrow-right" size={buttonSize * 10.4} color="white"/>
-            </Animated.View>
-        </Pressable>
-    );
+  return (
+    <Pressable
+      className='absolute top-[8%] items-center justify-center aspect-square rounded-full shadow-lg'
+      style={{
+        width: buttonSize * 16,
+        backgroundColor: buttonColor,
+        left: openSidebarWidth + closedSidebarWidth - (buttonSize * 16) / 2,
+      }}
+      onPress={toggleSidebar}
+    >
+      <Animated.View style={animatedStyle}>
+        <MaterialIcons name='keyboard-arrow-right' size={buttonSize * 10.4} color='white' />
+      </Animated.View>
+    </Pressable>
+  )
 }
 
 const Sidebar = ({ animatedValue }: { animatedValue: SharedValue<number> }) => {
-    // Sidebar and transition details
-    const { transitionDuration, transitionEasing } = useTransitionCustomization();
-    const { isSidebarOpen, openSidebarWidth, closedSidebarWidth } = useSidebarContext();
-    const enteringAnimation = FadeInLeft.duration(transitionDuration / 1.5).easing(transitionEasing);
-    const exitingAnimation = FadeOutLeft.duration(transitionDuration).easing(transitionEasing);
+  // Sidebar and transition details
+  const { transitionDuration, transitionEasing } = useTransitionCustomization()
+  const { isSidebarOpen, openSidebarWidth, closedSidebarWidth } = useSidebarContext()
+  const enteringAnimation = FadeInLeft.duration(transitionDuration / 1.5).easing(transitionEasing)
+  const exitingAnimation = FadeOutLeft.duration(transitionDuration).easing(transitionEasing)
 
-    // Sidebar slide interpolation
-    const sidebarStyle = useAnimatedStyle(() => ({
-        transform: [
-            { translateX: interpolate(animatedValue.value, [0, 1], [-openSidebarWidth, 0]) }
-        ],
-    }));
-    
-    // Button rotation interpolation (> when closed, < when open)
-    const buttonStyle = useAnimatedStyle(() => ({
-        transform: [
-            { rotate: `${interpolate(animatedValue.value, [0, 0.5, 1], [0, -90, -180])}deg` }
-        ],
-    }));
+  // Sidebar slide interpolation
+  const sidebarStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: interpolate(animatedValue.value, [0, 1], [-openSidebarWidth, 0]) }],
+  }))
 
-    return (
-        <Animated.View style={[
-            sidebarStyle,
-            {
-                height: Dimensions.get('window').height,
-                zIndex: 1, // Overlays over main content
-            }]}>
-            <View className="flex-1 justify-center bg-white" style={{
-                height: Dimensions.get('window').height,
-                width: openSidebarWidth + closedSidebarWidth,
-            }}>
+  // Button rotation interpolation (> when closed, < when open)
+  const buttonStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${interpolate(animatedValue.value, [0, 0.5, 1], [0, -90, -180])}deg` }],
+  }))
 
-                {/* Open/collapse button */}
-                <SidebarButton animatedStyle={buttonStyle}/>
-                
-                {/* Sidebar contents */}
-                <View className="flex-1 self-center justify-center items-center flex-col">
-                    {isSidebarOpen &&
-                    (<Animated.View style={{ position:'relative', zIndex: 2}} entering={enteringAnimation} exiting={exitingAnimation}>
+  return (
+    <Animated.View
+      style={[
+        sidebarStyle,
+        {
+          height: Dimensions.get('window').height,
+          zIndex: 1, // Overlays over main content
+        },
+      ]}
+    >
+      <View
+        className='flex-1 justify-center bg-white'
+        style={{
+          height: Dimensions.get('window').height,
+          width: openSidebarWidth + closedSidebarWidth,
+        }}
+      >
+        {/* Open/collapse button */}
+        <SidebarButton animatedStyle={buttonStyle} />
 
-                        {/* App icon */}
-                        <View className="justify-end" style={{ height: openSidebarWidth * .65 }}>
-                            <Image className="flex-row justify-between self-center pt-5"
-                            source={require('../assets/images/bb_icon.png')} style={{
-                                height: openSidebarWidth * .60,
-                                width: openSidebarWidth * .60,
-                            }} />
-                        </View>
-                    
-                        {/* Navigable screens */}
-                        <View className="flex-col items-start justify-between" style={{
-                            height: Dimensions.get('window').height * 0.45,
-                            width: openSidebarWidth,
-                            }}>
+        {/* Sidebar contents */}
+        <View className='flex-1 self-center justify-center items-center flex-col'>
+          {isSidebarOpen && (
+            <Animated.View
+              style={{ position: 'relative', zIndex: 2 }}
+              entering={enteringAnimation}
+              exiting={exitingAnimation}
+            >
+              {/* App icon */}
+              <View className='justify-end' style={{ height: openSidebarWidth * 0.65 }}>
+                <Image
+                  className='flex-row justify-between self-center pt-5'
+                  source={require('../assets/images/bb_icon.png')}
+                  style={{
+                    height: openSidebarWidth * 0.6,
+                    width: openSidebarWidth * 0.6,
+                  }}
+                />
+              </View>
 
-                            {/* Home */}
-                            <SidebarTab iconSet={Entypo} iconName="home" label="Home"/>
+              {/* Navigable screens */}
+              <View
+                className='flex-col items-start justify-between'
+                style={{
+                  height: Dimensions.get('window').height * 0.45,
+                  width: openSidebarWidth,
+                }}
+              >
+                {/* Home */}
+                <SidebarTab iconSet={Entypo} iconName='home' label='Home' />
 
-                            {/* Students */}
-                            <SidebarTab iconSet={FontAwesome6} iconName="user-large" iconSize={ dynamicIconSize() - 3 } label="Students"/>
+                {/* Students */}
+                <SidebarTab
+                  iconSet={FontAwesome6}
+                  iconName='user-large'
+                  iconSize={dynamicIconSize() - 3}
+                  label='Students'
+                />
 
-                            {/* Schedule */}
-                            <SidebarTab iconSet={MaterialCommunityIcons} iconName="calendar-multiple" label="Schedule"/>
+                {/* Schedule */}
+                <SidebarTab
+                  iconSet={MaterialCommunityIcons}
+                  iconName='calendar-multiple'
+                  label='Schedule'
+                />
 
-                            {/* Games */}
-                            <SidebarTab iconSet={MaterialCommunityIcons} iconName="gamepad-square-outline" label="Games"/>
+                {/* Games */}
+                <SidebarTab
+                  iconSet={MaterialCommunityIcons}
+                  iconName='gamepad-square-outline'
+                  label='Games'
+                />
 
-                            {/* Timer */}
-                            <SidebarTab iconSet={MaterialCommunityIcons} iconName="timer-sand" label="Timer"/>
+                {/* Timer */}
+                <SidebarTab iconSet={MaterialCommunityIcons} iconName='timer-sand' label='Timer' />
 
-                            {/* Settings */}
-                            <SidebarTab iconSet={FontAwesome} iconName="gear" label="Settings"/>
-                        
-                        </View>
+                {/* Settings */}
+                <SidebarTab iconSet={FontAwesome} iconName='gear' label='Settings' />
+              </View>
+            </Animated.View>
+          )}
 
-                    </Animated.View>)}
-                        
-                    {/* Spacer */}
-                    <View className="flex-1 flex-shrink-2 relative justify-end items-start" style={{ width: openSidebarWidth, zIndex: 1 }}>
-                        {isSidebarOpen &&
-                        (<Animated.View entering={enteringAnimation} exiting={exitingAnimation}>
+          {/* Spacer */}
+          <View
+            className='flex-1 flex-shrink-2 relative justify-end items-start'
+            style={{ width: openSidebarWidth, zIndex: 1 }}
+          >
+            {isSidebarOpen && (
+              <Animated.View entering={enteringAnimation} exiting={exitingAnimation}>
+                {/* Sign out */}
+                <SidebarTab
+                  iconSet={Entypo}
+                  iconName='log-out'
+                  iconSize={dynamicIconSize() - 1}
+                  label='Sign out'
+                  useActiveColor={false}
+                  tabWidth={openSidebarWidth * 0.7}
+                />
+              </Animated.View>
+            )}
+          </View>
 
-                            {/* Sign out */}
-                            <SidebarTab iconSet={Entypo} iconName="log-out" iconSize={ dynamicIconSize() - 1 } label="Sign out" useActiveColor={false} tabWidth={openSidebarWidth * .7}/>
-
-                        </Animated.View>)}
-                    </View>
-
-                    {/* Bottom padding */}
-                    <View className="flex-1 flex-shrink-1" style={{ maxHeight: Dimensions.get('window').height * 0.06 }}/>
-                        
-                </View>
-            </View>
-        </Animated.View>
-    );
+          {/* Bottom padding */}
+          <View
+            className='flex-1 flex-shrink-1'
+            style={{ maxHeight: Dimensions.get('window').height * 0.06 }}
+          />
+        </View>
+      </View>
+    </Animated.View>
+  )
 }
-export default Sidebar;
+export default Sidebar
