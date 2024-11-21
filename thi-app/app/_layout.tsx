16961f00246ed41ecdf4b53927ec68e2b2d6f2de
An Bui <61863411+AB-102@users.noreply.github.com>
2024-11-21 16:13:06 -0600

    Revert _layout.tsx

--- thi-app/app/_layout.tsx
+++ thi-app/app/_layout.tsx
@@ -1,14 +1,14 @@
-import '../global.css'
-import { useEffect } from 'react'
-import { View } from 'react-native'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { useFonts } from 'expo-font'
-import { SplashScreen, Stack } from 'expo-router'
+import "../global.css";
+import { useEffect } from "react";
+import { View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router'
+} from "expo-router";
 
 // export const unstable_settings = {
 //   // Ensure that reloading on `/modal` keeps a back button present.
@@ -16,35 +16,35 @@ export {
 // };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    Jost: require('../assets/fonts/Jost-VariableFont_wght.ttf'),
-  })
+    'Jost': require("../assets/fonts/Jost-VariableFont_wght.ttf")
+  });
 
   useEffect(() => {
     if (loaded || error) {
-      SplashScreen.hideAsync()
+      SplashScreen.hideAsync();
     }
-  }, [loaded, error])
+  }, [loaded, error]);
 
   if (!loaded && !error) {
-    return null
+    return null;
   }
 
-  return <RootLayoutNav />
+  return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
   return (
-    <GestureHandlerRootView className='flex-1'>
-      <View className='absolute inset-0'>
+    <GestureHandlerRootView className="flex-1">
+      <View className="absolute inset-0">
         <Stack>
-          <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
-          <Stack.Screen name='index' options={{ headerShown: false }} />
+          <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
       </View>
     </GestureHandlerRootView>
-  )
+  );
 }
