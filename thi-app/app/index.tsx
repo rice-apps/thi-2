16f4c0c04552f439d6467ee80cde4611dec1178d
trietmchau <trietmchau@gmail.com>
2025-02-08 13:35:10 -0600

    Fix wrong file path

--- thi-app/app/index.tsx
+++ thi-app/app/index.tsx
@@ -1,11 +1,10 @@
-import Login from '@/components/login';
-import React from 'react';
-import { View } from 'react-native';
+import { View } from "react-native";
+import Login from "../components/Login";
 
 export default function LoginPage() {
-  return (
-    <View className='flex-1 justify-center bg-white'>
-      <Login />
-    </View>
-  )
+    return (
+        <View className="flex-1 justify-center bg-white">
+            <Login />
+        </View>
+    );
 }
