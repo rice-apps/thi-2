f1ff3882ee1fb8c32455bfdc4166bc70697b84fb
Faith <faithzhng@gmail.com>
2025-03-01 15:40:01 -0600

    finished individual student page! -> for fully implemntation, will write api calls

--- thi-app/app/(drawer)/individual_student.tsx
+++ thi-app/app/(drawer)/individual_student.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity, FlatList} from "react-native";
+import { Text, View, TouchableOpacity, FlatList, ScrollView} from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import StudentForm from "@/screens/student_modals/StudentForm";
@@ -18,10 +18,6 @@ const Item = ({date}: ItemProps) => (
   </View>
 );
 
-type DataEntries = {
-  abc: any[];  // Change 'any' to a more specific type if needed
-  duration: any[];  // Same for this
-};
 
 
 const IndividualStudent = () => {
@@ -31,22 +27,24 @@ const IndividualStudent = () => {
   const [isModalVisible, setModalVisible] = useState(false);
   const [isFilterVisible, setFilterVisible] = useState(false);
   const [isExportVisible, setExportVisible] = useState(false);
+  const [dataEntriesABC, setDataEntriesABC] = useState<any[]>([]);
+  const [dataEntriesDuration, setDataEntriesDuration] = useState<any[]>([]);
 
-  const [dataEntries, setDataEntries] = useState<DataEntries>({
-    abc: [],
-    duration: []
-  });
-  
 
-  
 
-  //fetch from API whenever backend is done
-  // useEffect(() => {
-  //   setDataEntries({
-  //     id: studentId,
-  //     name: name,
-  //   });
-  // }, [studentId, name]);
+
+  //this will eventually be a API fetch call
+  const handleAddData = (newData: any) => {
+    if (newData.hasOwnProperty('behavior')) {
+      setDataEntriesABC((prevEntries) => [...prevEntries, newData]);
+    }
+    else{
+      setDataEntriesDuration((prevEntries) => [...prevEntries, newData]);
+    }
+
+    setModalVisible(false); 
+  };
+
 
 
 
@@ -64,16 +62,6 @@ const IndividualStudent = () => {
   const closeExport = () => {
     setExportVisible(false);
   };
-
-
-  const handleAddData = (newData: any, dataType: keyof DataEntries) => {
-    setDataEntries((prevEntries: DataEntries) => ({
-      ...prevEntries,
-      [dataType]: [...prevEntries[dataType], newData] 
-    }));
-    setModalVisible(false); 
-  };
-
 
 
 
@@ -94,15 +82,24 @@ const IndividualStudent = () => {
       <Text style={{ fontWeight: "bold", width: 100 }}>Consequence</Text> 
     </View>
   );
-
   const renderABCItem = ({ item }: { item: any }) => (
-    <View style={{ flexDirection: "row", justifyContent: "space-between", padding: 10 }}>
-      <Text>{item.field1}</Text> 
-      <Text>{item.field2}</Text>
+    <View style={{ flexDirection: "row", justifyContent: "flex-start", padding: 10, paddingTop: 15, paddingBottom: 15,  backgroundColor: "#FAFAFA",
+      // shadowColor: "#000",  
+      // shadowOffset: { width: 0, height:1},  
+      // shadowOpacity: 0.5,  
+      // shadowRadius: 4,  
+      // elevation: 5,  
+   }}>
+      <Text style={{width: 75 }}>3/1/2025</Text> 
+      <Text style={{width: 75 }}>3:15 PM</Text> 
+      <Text style={{width: 150 }}>Jane Summers</Text> 
+      <Text style={{width: 100 }}>{item.setting}</Text> 
+      <Text style={{width: 225 }}>{item.preIncident}</Text> 
+      <Text style={{width: 125 }}>{item.behavior}</Text> 
+      <Text style={{width: 100 }}>{item.consequence}</Text> 
     </View>
   );
-
-
+  
   const renderDurationHeader = () => (
     <View style={{ flexDirection: "row", justifyContent: "flex-start", padding: 10, paddingTop: 15, paddingBottom: 15, backgroundColor: "#F0F0F0",
         shadowColor: "#000",  
@@ -119,17 +116,21 @@ const IndividualStudent = () => {
       <Text style={{ fontWeight: "bold", width: 300 }}>Notes</Text> 
     </View>
   );
-
-
-  const renderDurationData = ({ item }: { item: any }) => (
-    <View style={{ flexDirection: "row", justifyContent: "space-between", padding: 10 }}>
-      <Text>{item.field1}</Text> 
-      <Text>{item.field2}</Text>
+  const renderDurationItem = ({ item }: { item: any }) => (
+    <View style={{ flexDirection: "row", justifyContent: "flex-start", padding: 10, paddingTop: 15, paddingBottom: 15, backgroundColor: "#FAFAFA",}}>
+      <Text style={{width: 100 }}>3/1</Text> 
+      <Text style={{width: 150 }}>{item.setting}</Text> 
+      <Text style={{width: 150 }}>{item.preIncident} </Text> 
+      <Text style={{width: 150 }}> {item.setting} - {item.preIncident} </Text> 
+      <Text style={{width: 200 }}>{item.postIncident}</Text> 
+      <Text style={{width: 300 }}>{item.notes}</Text> 
     </View>
   );
 
 
+
   return (
+    <ScrollView>
     <View className="flex-1 p-8">
      <TouchableOpacity onPress={backToStudentsPage} className="p-2 flex-row items-center">
           <AntDesign name="left" size={24} color="#105366" />
@@ -150,7 +151,7 @@ const IndividualStudent = () => {
       {/* ABC Behavior Data Section */}
       <View className="flex-row justify-between items-center mb-8">
         <Text className="text-2xl font-bold mb-4">ABC Behavior Data 
-          <Text className= "text-sm  text-black font-normal px-2"> {dataEntries.abc.length} Reports </Text>
+          <Text className= "text-sm  text-black font-normal px-2"> {dataEntriesABC.length} Reports </Text>
         </Text>
         <View className="flex-row space-x-4">
 
@@ -166,25 +167,20 @@ const IndividualStudent = () => {
         </View>
       </View>
 
-
-
-      {/* ABC Data Table*/}
-      <View className="mb-8">
-        {renderABCHeader()}
-
-        <FlatList
-        data={dataEntries.abc}
-        renderItem={({item}) => <Item date={item.date} />}
-        keyExtractor={item => item.id}
-      />
-
-        
+          {/* ABC Data Table*/}
+          <View className="mb-8">
+            {renderABCHeader()}
+            <FlatList
+            data={dataEntriesABC}
+            renderItem={renderABCItem}
+            keyExtractor={(item, index) => index.toString()}
+          />
       </View>
 
       {/* Duration Data Section */}
       <View className="flex-row justify-between items-center mb-8">
         <Text className="text-2xl font-bold mb-4">Duration Data 
-          <Text className= "text-sm  text-black font-normal px-2"> {dataEntries.duration.length} Reports </Text>
+          <Text className= "text-sm  text-black font-normal px-2"> {dataEntriesDuration.length} Reports </Text>
         </Text>
 
         <View className="flex-row space-x-4">
@@ -201,18 +197,25 @@ const IndividualStudent = () => {
         </View>
       </View>
 
-      {/* Table Placeholder */}
+      {/* Duration Data Table*/}
       <View>
         {renderDurationHeader()}
+        <FlatList
+        data={dataEntriesDuration}
+        renderItem={renderDurationItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
       </View>
 
+
       {/* Modals */}
-      <StudentForm visible={isModalVisible} onClose={closeModal} />
+      <StudentForm visible={isModalVisible} onClose={closeModal} onSubmit={handleAddData} />
       <Filter visible={isFilterVisible} onClose={closeFilter} />
       <ExcelExport visible={isExportVisible} onClose={closeExport} />
 
 
     </View>
+    </ScrollView>
   );
 };
 
